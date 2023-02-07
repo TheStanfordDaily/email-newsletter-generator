ece10b6ca3f0b6e07c20f0820d9385c95f61ebf1
Aditeya Shukla <aditeyashukla@gmail.com>
2023-02-06 22:45:04 -0800

    Ads support added

--- make_digest.py
+++ make_digest.py
@@ -16,6 +16,7 @@ ENDPOINT = "https://stanforddaily.com/wp-json/wp/v2/posts"
 CATEGORY_PAGES = {
     "FEATURED": "",
     "EDITOR'S PICK": "",
+    "AD":"",
     "NEWS": "https://stanforddaily.com/category/news/",
     "OPINIONS": "https://stanforddaily.com/category/opinions/",
     "SPORTS": "https://stanforddaily.com/category/sports/",
@@ -160,12 +161,15 @@ class Article:
 
 
 class Section:
-    def __init__(self, urls, name=None, featured=False, editorsNote=None, editor=None):
+    def __init__(self, urls, name=None, featured=False, editorsNote=None, editor=None, ad_weblink=None,ad_src=None, ad_alt=None):
         self.name = name
         self.editorsNote = editorsNote
         self.editor = editor
-
+        self.ad_weblink=ad_weblink
+        self.ad_src=ad_src 
+        self.ad_alt=ad_alt
         slugs = [x.split('/')[6] for x in urls]
+        
         response = requests.get(ENDPOINT, params={"slug": ','.join(slugs)})
         data = response.json()
 
@@ -180,7 +184,7 @@ class Section:
     def render(self):
         if self.name is None or "CARTOON" in self.name or self.featured:
             title = ""
-        if "EDITOR'S PICK" in self.name:
+        elif "EDITOR'S PICK" in self.name:
             title = f"""
                     <tr><td align="left" class="em_defaultlink" style="font-family: 'Open Sans', Arial, sans-serif;font-size: 17px;line-height: 21px;color: #8c1514;font-weight: 900;padding-bottom: 5px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;border-collapse: collapse;" valign="top">
                       <a href="{formatted_url(CATEGORY_PAGES[self.name])}" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;border-collapse: collapse;color: inherit !important;text-decoration: none !important;">
@@ -203,6 +207,16 @@ class Section:
                         </td>
                     </tr>
                     
+                    """
+        elif self.name == "AD":
+            title = f"""
+                    <tr>
+                        <td style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;border-collapse: collapse; padding-bottom: 25px;">
+                        <a href={self.ad_weblink} style="text-decoration: none;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;border-collapse: collapse;" target="_blank">
+                            <img alt="{self.ad_alt}" border="0" class="em_full_img" height="180" src={self.ad_src} style="display: block;font-family: Arial, sans-serif;font-size: 20px;line-height: 25px;color: #424242;max-width: 520px;border: 0 !important;height: auto;outline: none !important;text-decoration: none;-ms-interpolation-mode: bicubic;" width="600">
+                        </a>
+                        </td>
+                    </tr>
                     """
         else:
             title = f"""
@@ -233,6 +247,20 @@ def sections_from_file(directory):
     section_links = [[]]
 
     try:
+        editors_note_idx = lines.index("AD")
+        ad_weblink = lines[editors_note_idx+1]
+        ad_src= lines[editors_note_idx+2]
+        ad_alt= lines[editors_note_idx+3]
+        lines.pop(editors_note_idx+3)
+        # lines.pop(editors_note_idx+2)
+        lines.pop(editors_note_idx+1)
+        # lines.pop(editors_note_idx)
+    except ValueError:
+        ad_weblink = None
+        ad_src = None
+        ad_alt = None
+
+    try:
         editors_note_idx = lines.index("EDITOR'S PICK")
         editors_note = lines[editors_note_idx+2]
         editor = lines[editors_note_idx+3]
@@ -258,7 +286,7 @@ def sections_from_file(directory):
     for index, group in enumerate(section_links):
         name = section_names[index]
         featured = name == "FEATURED"
-        yield Section(group, name=name, featured=featured, editor=editor, editorsNote=editors_note)
+        yield Section(group, name=name, featured=featured, editor=editor, editorsNote=editors_note, ad_weblink=ad_weblink,ad_src=ad_src, ad_alt=ad_alt )
 
 
 if __name__ == "__main__":
